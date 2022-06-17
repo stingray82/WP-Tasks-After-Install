285e6c542eac087b1b152d99f852037f3933b791
stingray82 <11146912+stingray82@users.noreply.github.com>
2022-06-17 23:03:24 +0100

    Init

---
+++ readme.txt
@@ -0,0 +1,74 @@
+=== WP Tasks After Install ===
+Contributors: oabadfol, valhallawp, ohyeahdev, fernandoaureonet
+Donate link: https://www.valhallawp.com/plugins/wp-tasks-after-install
+Tags: default content, remove, starter, installation
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Stable tag: trunk
+Requires at least: 3.0
+Tested up to: 4.9.8
+
+A WordPress plugin that perform some necessary tasks for after WordPress install.
+
+== Description ==
+This plugin if for use only once in each WordPress installation becouse it perform this initial task just after WordPress installation:
+
+   * Removes the default "Hello World" post.
+   * Removes the default page "Example page".
+   * Changes the name and slug of Uncategorized default category to "Blog".
+   * Sets the permalink structure to "%/postname%/.
+   * Removes "Hello Dolly" and "Akismet" plugins.
+   * Disables comments and pings.
+   * Deletes the wp-config-sample.php file.
+   * Deletes readme.html file
+   * Delete all themes except the default one.
+   * Deactivates the plugin itself.
+   
+Note: Dont forget uninstall this plugin after finished.
+
+More information and request: https://www.oscarabadfolgueira.com/plugins/wp-tasks-after-install
+
+== Installation ==
+
+1. Visit your admin section and click Plugins -> Add New.  Then search for "WP Tasks After Install”.
+
+2. After activate, the plugin perform all tasks.
+
+3. When finished, the plugin is deactivated. Yo need to uninstall it.
+
+== Screenshots ==
+
+== Changelog ==
+
+= 1.1 =
+* Contributors
+
+= 1.0 =
+* First version of plugin
+
+= 1.0.1 =
+* Minor change in readme.txt
+
+= 1.0.2 =
+* Added Internazionalization
+
+= 1.0.3 =
+* Some minor changes in readme file
+
+= 1.0.4 =
+* Added Spanish-Spain translation
+
+= 1.0.5 =
+* Added deletion of readme.html file.
+
+= 1.0.6 =
+* Minor fixes
+
+= 1.0.7 =
+* Add the task to remove Akismet.
+
+= 1.0.8 =
+* Reguar changes.
+
+= 1.0.9 =
+* Minor change
---
+++ wp-tasks-after-install.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * Plugin Name: WP Tasks After Install Modified
+ * Plugin URI: https://github.com/stingray82/WP-Tasks-After-Install
+ * Description: Performs a number of necessary tasks after installing WordPress.
+ * Author: Oh Yeah Devs / Stingray82
+ * Author URI: https://github.com/stingray82/WP-Tasks-After-Install
+ * Version: 1.91
+ * License: GPLv2 or later
+ * Text Domain: wp-tasks-after-install
+ * Domain Path: /languages/
+ */
+
+// Go away!!
+if ( ! defined( 'WPINC' ) ) {
+     die;
+}
+
+// Adds plugin internationalization
+function oaf_wptai_i18n() {
+	load_plugin_textdomain( 'wp-tasks-after-install', FALSE, basename(dirname( __FILE__ ) ) . '/languages/' );
+}//end plugin_name_i18n()
+
+add_action( 'plugins_loaded', 'oaf_wptai_i18n' );
+
+add_action( 'admin_init', 'oaf_wptai_remove_default_post');
+add_action( 'admin_init', 'oaf_wptai_remove_default_page'); // Removed the privacy page
+add_action( 'admin_init', 'oaf_wptai_change_uncategorized');
+add_action( 'admin_init', 'oaf_wptai_set_permalink_postname' );
+add_action( 'admin_init', 'oaf_wptai_delete_plugins' );
+add_action( 'admin_init', 'oaf_wptai_disable_comments_and_pings' ); // Search Enginee Added
+add_action( 'admin_init', 'oaf_wptai_delete_config_sample_file' );
+add_action( 'admin_init', 'oaf_wptai_delete_readme_html_file' );
+add_action( 'admin_init', 'oaf_wptai_delete_themes' );
+add_action( 'admin_init', 'oaf_wptai_deactivate_this_plugin' );
+
+
+// Remove default post 'Hello Word'
+function oaf_wptai_remove_default_post() {
+	
+	if ( FALSE === get_post_status( 1 ) ) {
+	   	// The post does not exist - do nothing.		
+	} else {
+	   	wp_delete_post(1);
+	}
+	
+} // end of oaf_wptai_remove_default_post() function.
+
+// Remove the default example page
+function oaf_wptai_remove_default_page() {
+	
+	if ( FALSE === get_post_status( 2 ) ) {
+	   	// The page does not exist - do nothing.		
+	} else {
+	   	wp_delete_post(2);
+	}
+	if ( FALSE === get_post_status( 3 ) ) {
+	   	// The page does not exist - do nothing.		
+	} else {
+	   	wp_delete_post(3);
+	}
+	
+} // end of oaf_wptai_remove_default_page() function
+
+
+// Change the name and slug of default category to news
+function oaf_wptai_change_uncategorized() {
+	
+	$term = term_exists( __('Uncategorized', 'wp-tasks-after-install', 'wp-tai'), 'category'); // check if 'uncategorized' category exists
+	
+	if ($term !== 0 && $term !== null) {  // if exists change name and slug
+	  wp_update_term(1, 'category', array(
+	  	'name' => __( 'General', 'wp-tasks-after-install', 'wp-tai' ),
+	  	'slug' => __( 'general', 'wp-tasks-after-install', 'wp-tai' )
+	  ));
+	}
+	
+} // end of oaf_wptai_change_uncategorized() function.
+
+
+// Set permlinks to postname  /%postname%/
+function oaf_wptai_set_permalink_postname() {
+	
+    global $wp_rewrite;
+    $wp_rewrite->set_permalink_structure( '/%postname%/' );
+    
+} // end of oaf_wptai_set_permalink_postname() function.
+
+
+// remove hello world and akismet plugins
+function oaf_wptai_delete_plugins() {
+	
+    $plugins = array( 'hello.php', 'akismet/akismet.php' );
+	delete_plugins( $plugins );
+	
+} // end of oaf_wptai_delete_plugins function.
+
+// Disable comments, Search Enginees and trackbacks
+function oaf_wptai_disable_comments_and_pings() {
+
+	// Disable pings
+	if( '' != get_option( 'default_ping_status' ) ) {
+		update_option( 'default_ping_status', '' );
+	} // end if
+
+	// Disable comments
+	if( '' != get_option( 'default_comment_status' ) ) {
+		update_option( 'default_comment_status', '' );
+	} // end if
+	
+	// Discourage search engines from indexing this site
+	if( '' != get_option( 'blog_public' ) ) {
+		update_option( 'blog_public', '' );
+	} // end if
+
+} // end oaf_wptai_disable_comments_and_pings() function.
+
+
+// Delete wp-config-sample.php file
+function oaf_wptai_delete_config_sample_file() {
+	
+	$url_config_sample = "wp-config-sample.php";
+	$abspath=$_SERVER['DOCUMENT_ROOT'];
+	$file_url = $abspath . '/' . $url_config_sample;
+	if (file_exists($file_url)) {
+	    unlink($file_url);
+	}
+
+} // end of oaf_wptai_delete_config_sample_file() function.
+
+// Delete readme.html file
+function oaf_wptai_delete_readme_html_file() {
+	
+	$url_readme_html = "readme.html";
+	$abspath=$_SERVER['DOCUMENT_ROOT'];
+	$file_url = $abspath . '/' . $url_readme_html;
+	if (file_exists($file_url)) {
+	    unlink($file_url);
+	}
+
+} // end of oaf_wptai_delete_readme_html_file() function.
+
+
+// Remove unactivated themes
+function oaf_wptai_delete_themes() {
+
+	// The current themes.
+	$installed_themes = wp_get_themes();
+
+	// The themes we want to keep (delete the others).
+	$theme_data = wp_get_theme();
+	$current_theme = $theme_data->get( 'TextDomain' );
+
+	$themes_to_keep = array( $current_theme );
+
+	// Loop through installed themes.
+	foreach ( $installed_themes as $theme ) {
+
+		// The name of the theme.
+		$name = $theme->get_template();
+
+		// If it's not one we want to keep...
+		if ( ! in_array( $name, $themes_to_keep ) ) {
+			$stylesheet = $theme->get_stylesheet();
+
+			// Delete the theme.
+			delete_theme( $stylesheet, false );
+		}
+	} // end of foreach - themes
+	
+} // end of oaf_wptai_delete_themes() function.
+
+// Deactivate this plugin.
+function oaf_wptai_deactivate_this_plugin() {
+
+	if ( !function_exists( 'deactivate_plugins' ) ) { 
+	    require_once ABSPATH . '/wp-admin/includes/plugin.php'; 
+	} 
+	
+	deactivate_plugins( plugin_basename( __FILE__ ) );
+	
+} // end of oaf_wptai_deactivate_this_plugin() function.
